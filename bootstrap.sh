a925cc6f53817de49790126bc62709f7d57a0aff
Matt Clements <matt@mattclements.co.uk>
2014-10-20 19:00:58 +0100

    Updating from iMac Setup on Yosemite

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync --exclude ".git/" --exclude ".gitkeep" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
@@ -14,4 +14,4 @@ else
 	fi
 fi
 unset doIt
-source ~/.bash_profile
+source ~/.zshrc
